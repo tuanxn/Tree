da9d30a045c90d2dda53e3b14bff38b9a1e1032f
tuanxn <tuan.uci@gmail.com>
2020-07-05 17:26:19 -0700

    Add project files.

---
+++ Tree/Tree.cpp
@@ -0,0 +1,214 @@
+// Student ID: 20274909
+
+#include <iostream>
+#include <string>
+#include <sstream>
+#include "Tree.h"
+
+using namespace std;
+
+int main()
+{
+   Tree temp = Tree();
+   temp.make_special_config_1({ "AABA", "ABAB", "ABBA", "BABA", "COBO", "COCO", "CODO", "COFO", "COGO", "COHO", "COJO",
+"COKO", "DIBI", "DIDI", "DIFI", "DIGI", "DIHI", "DIJI", "DIKI", "DILI" });
+
+   Tree::Node temp_node = temp.create_node("test");
+   temp_node.insert_child(new Tree::Node("child"));
+   temp_node.insert_sibling(new Tree::Node("sib"));
+   temp_node.insert_sibling(new Tree::Node("sib2"));
+
+   Tree::Node new_node = temp_node;
+
+   Tree::Node diff_node = temp.create_node("diff");
+
+   cout << new_node.is_equal(&new_node, &diff_node);
+
+   cout << new_node.is_equal(&new_node, &temp_node);
+
+
+}
+
+Tree::Node* Tree::Node::insert_sibling(Node* p) {
+   if (_sibling == nullptr) {
+      _sibling = p;
+      return p;
+   }
+
+   this->_sibling->insert_sibling(p);
+
+   cout << "insert sibling";
+
+   return p;
+}
+
+Tree::Node* Tree::Node::insert_child(Node* p) {
+   if (_child == nullptr) {
+      _child = p;
+      return p;
+   }
+
+   this->_child->insert_sibling(p);
+
+   cout << "insert child";
+
+   return p;
+}
+
+const Tree::Node& Tree::Node::operator=(const Tree::Node& that) {
+   // TODO
+   
+   if (this != &that) {
+      Node* temp = new Node(that.get_data());
+      if (that._child == nullptr) {
+         temp->_child = nullptr;
+      }
+      else {
+         temp->_child = new Node(*that._child);
+      }
+      if (that._sibling == nullptr) {
+         temp->_sibling = nullptr;
+      }else {
+         temp->_sibling = new Node(*that._sibling);
+      }
+      this->set_data(temp->get_data());
+      this->_child = temp->_child;
+      this->_sibling = temp->_sibling;
+   }
+   cout << "assign node";
+   return *this;
+}
+
+Tree::Node::Node(const Node& that) {
+   *this = that;
+}
+
+bool Tree::Node::is_equal(const Node* p1, const Node* p2) {
+   if (p1 == nullptr && p2 == nullptr) {
+      return true;
+   }
+   else if (*p1 == *p2) {
+      return true;
+   }
+   return false;
+}
+
+bool Tree::Node::operator==(const Node& that) const {
+   bool result = true;
+   if (this->_child == nullptr && that._child == nullptr && this->_sibling == nullptr && that._sibling == nullptr) {
+      return true;
+   }
+   else if (this->_child == nullptr && that._child != nullptr) {
+      return false;
+   }
+   else if (this->_child != nullptr && that._child == nullptr) {
+      return false;
+   }
+   else if (this->_sibling == nullptr && that._sibling != nullptr) {
+      return false;
+   }
+   else if (this->_sibling != nullptr && that._sibling == nullptr) {
+      return false;
+   }
+   else {
+      result = (*this->_child == *that._child);
+      if (result) {
+         result = (*this->_sibling == *that._sibling);
+      }
+   }
+   return result;
+}
+
+bool Tree::Node::operator!=(const Node& that) const {
+   return (!(this == &that));
+}
+
+string Tree::Node::to_string() const {
+   string output = this->get_data() + " :";
+   if (this->_child != nullptr) {
+      Node* temp = this->_child;
+      output += " " + temp->get_data();
+      while (temp->_sibling != nullptr) {
+         output += " " + temp->_sibling->get_data();
+         temp = temp->_sibling;
+      }
+   }
+
+   output += "\n";
+
+   if (this->_child != nullptr) {
+      output += "# Child of " + this->get_data() + "\n";
+      output += this->_child->to_string();
+   }
+
+   if (this->_sibling != nullptr) {
+      output += "# Next sib of " + this->get_data() + "\n";
+      output += this->_sibling->to_string();
+   }
+   return output;
+}
+
+Tree::Tree(const Tree& that) {
+   *this = that;
+}
+Tree& Tree::operator=(const Tree& that) {
+   if (this != &that) {
+      Tree temp = Tree();
+      *temp._root = *that._root;
+      *this = temp;
+   }
+   cout << "assign node";
+   return *this;
+}
+
+string Tree::to_string() const {
+   string output = "";
+   output += "# Tree rooted at " + this->_root->get_data() + "\n";
+   output += "# The following lines are of the form:\n";
+   output += "#   node: child1 child2...\n";
+   output += this->_root->to_string();
+   output += "# End of Tree\n";
+   return output;
+}
+
+void Tree::make_special_config_1(const vector<string>& names) {
+   Node* root_sib;
+   Node* root_sib_child;
+
+   this->_root->insert_sibling(new Node(names[0]));
+   root_sib = this->_root->_sibling; // AABA
+   root_sib->insert_child(new Node(names[4]));
+   root_sib_child = root_sib->_child; // COBO
+   root_sib_child->insert_child(new Node(names[12]));
+   root_sib_child->insert_sibling(new Node(names[5]));
+   root_sib_child = root_sib_child->_sibling; // COCO
+   root_sib_child->insert_child(new Node(names[13]));
+
+   root_sib->insert_sibling(new Node(names[1]));
+   root_sib = root_sib->_sibling; // ABAB
+   root_sib->insert_child(new Node(names[6]));
+   root_sib_child = root_sib->_child; // CODO
+   root_sib_child->insert_child(new Node(names[14]));
+   root_sib_child->insert_sibling(new Node(names[7]));
+   root_sib_child = root_sib_child->_sibling; // COFO
+   root_sib_child->insert_child(new Node(names[15]));
+
+   root_sib->insert_sibling(new Node(names[2]));
+   root_sib = root_sib->_sibling; // ABBA
+   root_sib->insert_child(new Node(names[8]));
+   root_sib_child = root_sib->_child; // COGO
+   root_sib_child->insert_child(new Node(names[16]));
+   root_sib_child->insert_sibling(new Node(names[9]));
+   root_sib_child = root_sib_child->_sibling; // COHO
+   root_sib_child->insert_child(new Node(names[17]));
+
+   root_sib->insert_sibling(new Node(names[3]));
+   root_sib = root_sib->_sibling; // BABA
+   root_sib->insert_child(new Node(names[10]));
+   root_sib_child = root_sib->_child; // COJO
+   root_sib_child->insert_child(new Node(names[18]));
+   root_sib_child->insert_sibling(new Node(names[11]));
+   root_sib_child = root_sib_child->_sibling; // COKO
+   root_sib_child->insert_child(new Node(names[19]));
+
+}
---
+++ Tree/Tree.h
@@ -0,0 +1,62 @@
+// Student ID: 20274909
+
+#pragma once
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Tree {
+//private:
+public:
+   struct Node { // Inner class
+      std::string _data;
+      Node* _sibling, * _child;
+      static bool is_equal(const Node* p1, const Node* p2);
+      Node(std::string s = "") { _data = s; _sibling = nullptr; _child = nullptr; }
+      Node(const Node& that);
+      const Node& operator=(const Node& that); // Deep clone
+      ~Node() {
+         while (this->_sibling != nullptr) {
+            delete this->_sibling;
+            this->_sibling = nullptr;
+         }
+         while (this->_child != nullptr) {
+            delete this->_child;
+            this->_child = nullptr;
+         }
+      }
+      std::string get_data() const { return _data; }
+      void set_data(std::string s) { _data = s; }
+      Node* insert_sibling(Node* p);
+      Node* insert_child(Node* p);
+      std::string to_string() const;
+      bool operator==(const Node& that) const;
+      bool operator!=(const Node& that) const;
+   };
+   Node* _root;
+//public:
+   Tree() { _root = new Node("ROOT"); }
+   ~Tree() { delete _root; _root = nullptr; }
+   Tree(const Tree& that);
+   Tree& operator=(const Tree& that); // Deep clone
+   std::string to_string() const;
+   void make_special_config_1(const std::vector<std::string>& names);
+   bool operator==(const Tree& that) const {
+      return this->_root->is_equal(_root, that._root);
+   }
+   bool operator!=(const Tree& that) const {
+      return (!(this == &that));
+   }
+   friend std::ostream& operator<<(std::ostream& os, const Tree& tree) {
+      return os; // TODO
+   };
+   friend class Tests; // Don't remove this line
+
+   // comment out below
+
+   Node create_node(string str) {
+      return Node(str);
+   }
+
+};
